9a524dc000715d0871b52e141f695824c89ea453
Gabriel de Paula Santos <gabriel.santos@outlook.com>
2021-02-14 17:21:23 -0300

    Creation of the solution with the API project and all the Class libraries that will be used

---
+++ Models.WookieBooks/Models.WookieBooks/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Models.WookieBooks
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public byte[] CoverImage { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ Services.WookieBooks/Services.WookieBooks/BooksService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Services.WookieBooks
+{
+    public class BooksService
+    {
+    }
+}
