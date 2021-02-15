4cb82d0f6f3bc2044d747733d075dfb0936dd0c5
Gabriel de Paula Santos <gabriel.santos@outlook.com>
2021-02-14 23:01:40 -0300

    Creating Models

---
+++ Models.WookieBooks/Models.WookieBooks/Dto/CreateBookDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.WookieBooks.Dto
+{
+    public class CreateBookDto
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public byte[] CoverImage { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
