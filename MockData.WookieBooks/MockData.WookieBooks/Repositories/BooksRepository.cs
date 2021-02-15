6deb7dc762d798e1fbe26c8031a616a66cc85477
Gabriel de Paula Santos <gabriel.santos@outlook.com>
2021-02-14 23:02:14 -0300

    Creating Repositories

--- MockData.WookieBooks/MockData.WookieBooks/Repositories/BooksRepository.cs
+++ MockData.WookieBooks/MockData.WookieBooks/Repositories/BooksRepository.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Models.WookieBooks;
+using System.Linq;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace MockData.WookieBooks.Repositories
 {
-    class BooksRepository
+    public class BooksRepository : BaseRepository<Book>
     {
+        public BooksRepository(AppDbContext appDbContext) 
+            : base(appDbContext)
+        {
+        }
+
+        public override Book Get(int key)
+        {
+            return Context.Books
+                .Include(b => b.CreatedBy)
+                .Where(b => b.Id == key)
+                .SingleOrDefault();
+        }
+
+        public override List<Book> GetAll()
+        {
+            return Context.Books
+                .Include(b => b.CreatedBy)
+                .ToList();
+        }
     }
 }
