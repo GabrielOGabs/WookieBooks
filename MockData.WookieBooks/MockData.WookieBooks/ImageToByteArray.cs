2c603e0454b7feee4cc5f9e2b08f8ec96d7efff9
Gabriel de Paula Santos <gabriel.santos@outlook.com>
2021-02-16 15:02:29 -0300

    Added ImageToByteArray helper on the mocked data

---
+++ MockData.WookieBooks/MockData.WookieBooks/ImageToByteArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace MockData.WookieBooks
+{
+    internal class ImageToByteArray
+    {
+        private readonly string _basePath;
+
+        public ImageToByteArray()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            _basePath = Path.GetDirectoryName(path);
+        }
+
+        public byte[] GetImageBytes(string imageName)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var imagePath = Path.Combine(_basePath, "CoverImages", imageName);
+                var image = new Bitmap(imagePath);
+
+                image.Save(memoryStream, ImageFormat.Jpeg);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
--- MockData.WookieBooks/MockData.WookieBooks/MockDataInitializer.cs
+++ MockData.WookieBooks/MockData.WookieBooks/MockDataInitializer.cs
@@ -14,6 +14,8 @@ namespace MockData.WookieBooks
             using (var context = new AppDbContext(serviceProvider
                 .GetRequiredService<DbContextOptions<AppDbContext>>()))
             {
+                var imageToByteArray = new ImageToByteArray();
+
                 var adminUser = new User
                 {
                     FullName = "API Administrator",
@@ -46,7 +48,7 @@ namespace MockData.WookieBooks
                         Title = "Harry Potter and the Philosopher's Stone",
                         Description = "Escape to Hogwarts with the unmissable series that has sparked a lifelong reading journey for children and families all over the world. The magic starts here.",
                         Author = "J.K. Rowling",
-                        CoverImage = new byte[0],
+                        CoverImage = imageToByteArray.GetImageBytes("HarryPotter1.jpg"),
                         Price = 11.20m,
                         CreatedBy = hpUser,
                     },
@@ -55,7 +57,7 @@ namespace MockData.WookieBooks
                         Title = "Harry Potter and the Chamber of Secrets",
                         Description = "Harry Potter's summer has included the worst birthday ever, doomy warnings from a house-elf called Dobby, and rescue from the Dursleys by his friend Ron Weasley in a magical flying car!",
                         Author = "J.K. Rowling",
-                        CoverImage = new byte[0],
+                        CoverImage = imageToByteArray.GetImageBytes("HarryPotter2.jpg"),
                         Price = 12.38m,
                         CreatedBy = hpUser,
                     },
@@ -64,7 +66,7 @@ namespace MockData.WookieBooks
                         Title = "Harry Potter and the Prisoner of Azkaban",
                         Description = "When the Knight Bus crashes through the darkness and screeches to a halt in front of him, it's the start of another far from ordinary year at Hogwarts for Harry Potter.",
                         Author = "J.K. Rowling",
-                        CoverImage = new byte[0],
+                        CoverImage =  imageToByteArray.GetImageBytes("HarryPotter3.jpg"),
                         Price = 12.38m,
                         CreatedBy = hpUser,
                     },
@@ -73,7 +75,7 @@ namespace MockData.WookieBooks
                         Title = "Harry Potter and the Goblet of Fire",
                         Description = "The Triwizard Tournament is to be held at Hogwarts. Only wizards who are over seventeen are allowed to enter – but that doesn't stop Harry dreaming that he will win the competition.",
                         Author = "J.K. Rowling",
-                        CoverImage = new byte[0],
+                        CoverImage =  imageToByteArray.GetImageBytes("HarryPotter4.jpg"),
                         Price = 31.68m,
                         CreatedBy = hpUser,
                     },
@@ -82,7 +84,7 @@ namespace MockData.WookieBooks
                         Title = "Harry Potter and the Order of the Phoenix",
                         Description = "Dark times have come to Hogwarts. After the Dementors' attack on his cousin Dudley, Harry Potter knows that Voldemort will stop at nothing to find him.",
                         Author = "J.K. Rowling",
-                        CoverImage = new byte[0],
+                        CoverImage =  imageToByteArray.GetImageBytes("HarryPotter5.jpg"),
                         Price = 15.84m,
                         CreatedBy = hpUser,
                     },
@@ -91,7 +93,7 @@ namespace MockData.WookieBooks
                         Title = "Harry Potter and the Half-Blood Prince",
                         Description = "When Dumbledore arrives at Privet Drive one summer night to collect Harry Potter, his wand hand is blackened and shrivelled, but he does not reveal why.",
                         Author = "J.K. Rowling",
-                        CoverImage = new byte[0],
+                        CoverImage =  imageToByteArray.GetImageBytes("HarryPotter6.jpg"),
                         Price = 17.82m,
                         CreatedBy = hpUser,
                     },
@@ -100,7 +102,7 @@ namespace MockData.WookieBooks
                         Title = "Harry Potter and the Deathly Hallows",
                         Description = "As he climbs into the sidecar of Hagrid's motorbike and takes to the skies, leaving Privet Drive for the last time, Harry Potter knows that Lord Voldemort and the Death Eaters are not far behind.",
                         Author = "J.K. Rowling",
-                        CoverImage = new byte[0],
+                        CoverImage =  imageToByteArray.GetImageBytes("HarryPotter7.jpg"),
                         Price = 17.82m,
                         CreatedBy = hpUser,
                     },
@@ -109,7 +111,7 @@ namespace MockData.WookieBooks
                         Title = "A Brief History of Time",
                         Description = "This landmark book is for those of us who prefer words to equations; this is the story of the ultimate quest for knowledge, the ongoing search for the secrets at the heart of time and space.",
                         Author = "Stephen W. Hawking",
-                        CoverImage = new byte[0],
+                        CoverImage = imageToByteArray.GetImageBytes("stephen1.jpg"),
                         Price = 19.80m,
                         CreatedBy = shUser,
                     },
@@ -118,7 +120,7 @@ namespace MockData.WookieBooks
                         Title = "The Grand Design",
                         Description = "When and how did the universe begin? Why are we here? What is the nature of reality? Is the apparent “grand design” of our universe evidence of a benevolent creator who set things in motion—or does science offer another explanation?",
                         Author = "Stephen W. Hawking",
-                        CoverImage = new byte[0],
+                        CoverImage = imageToByteArray.GetImageBytes("stephen2.jpg"),
                         Price = 10.99m,
                         CreatedBy = shUser,
                     }
@@ -128,7 +130,7 @@ namespace MockData.WookieBooks
                         Title = "Black Holes and Baby Universes: And Other Essays",
                         Description = "In his phenomenal bestseller A Brief History of Time, Stephen Hawking literally transformed the way we think about physics, the universe, reality itself. In these thirteen essays and one remarkable extended interview, the man widely regarded as the most brilliant theoretical physicist since Einstein returns to reveal an amazing array of possibilities for understanding our universe.",
                         Author = "Stephen W. Hawking",
-                        CoverImage = new byte[0],
+                        CoverImage = imageToByteArray.GetImageBytes("stephen3.jpg"),
                         Price = 10.99m,
                         CreatedBy = shUser,
                     }
